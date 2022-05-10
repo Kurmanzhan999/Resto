7797d9bfc871d13a7a90cd6fa3b63bc73f05b626
kurmanzhan Muhtarova <kurmanzhanmuhtarova@gmail.com>
2022-05-10 12:43:57 -1000

    finished

--- js/index.js
+++ js/index.js
@@ -5,10 +5,27 @@ menu.onclick = () => {
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 };
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
 
 window.onscroll = () => {
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
+
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove('active');
+        document
+          .querySelector('header .navbar a[href*=' + id + ']')
+          .classList.add('active');
+      });
+    }
+  });
 };
 document.querySelector('#search-icon').onclick = () => {
   document.querySelector('#search-form').classList.toggle('active');
@@ -29,3 +46,34 @@ var swiper = new Swiper('.home-slider', {
   },
   loop: true,
 });
+
+var swiper = new Swiper('.review-slider', {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 6500,
+    disableOnInteraction: false,
+  },
+
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesperView: 1,
+    },
+    640: { slidesperView: 2 },
+    768: {
+      slidesperView: 2,
+    },
+    1024: {
+      slidesperView: 3,
+    },
+  },
+});
+
+function loader() {
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+window.onload = fadeOut;
